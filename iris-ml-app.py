e135e1d8c39122e0ce3f7a81bceeae5ae24acb8f
meoramsyar <111936981+meoramsyar@users.noreply.github.com>
2022-08-24 15:09:53 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -51,4 +51,4 @@ st.write(prediction_proba)
 
 st.image("https://www.gardendesign.com/pictures/images/900x705Max/site_3/iris-louisiana-black-gamecock-iris-beardless-louisiana-iris-shutterstock-com_12592.jpg", caption='Credit to GardenDesign')
 
-st.image("hhttps://unsplash.com/photos/OBtrCoiKlZo")
+st.image("https://unsplash.com/photos/OBtrCoiKlZo")
