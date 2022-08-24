2852f99bcc2df8fa92603dc15ab7570a814c6097
meoramsyar <111936981+meoramsyar@users.noreply.github.com>
2022-08-24 11:58:55 +0800

    Update myfourthapp.py

--- myfourthapp.py
+++ myfourthapp.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import time
 
-st.header("My first Streamlit App")
+st.header("My fourth Streamlit App")
 
 option = st.sidebar.selectbox(
     'Select a mini project',
