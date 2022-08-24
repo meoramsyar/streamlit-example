dfe365e53a3e455de31120fd033ce634ca38bcce
meoramsyar <111936981+meoramsyar@users.noreply.github.com>
2022-08-24 13:30:39 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
@@ -29,9 +29,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+iris = https://raw.githubusercontent.com/meoramsyar/streamlit-example/main/iris.csv
+
+
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -40,10 +40,10 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write("Iris-setosa", "Iris-versicolor", "Iris-virginica")
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
+
 #st.write(prediction)
 
 st.subheader('Prediction Probability')
