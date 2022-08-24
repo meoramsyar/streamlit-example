7c95ad354d1c6f261e825b78aebca02166c806fe
meoramsyar <111936981+meoramsyar@users.noreply.github.com>
2022-08-24 15:00:18 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -50,3 +50,5 @@ st.subheader('Prediction Probability')
 st.write(prediction_proba)
 
 st.image("https://www.gardendesign.com/pictures/images/900x705Max/site_3/iris-louisiana-black-gamecock-iris-beardless-louisiana-iris-shutterstock-com_12592.jpg", caption='Credit to GardenDesign')
+
+st.image("https://github.com/meoramsyar/streamlit-example/blob/main/iris%20flower.jpg")
